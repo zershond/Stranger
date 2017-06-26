8f817a520803d3da24187ccdea275e8f7e1e1f2f
czc <837561690@qq.com>
2017-06-26 15:31:53 +0800

    md文件

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by zershond on 2017/6/26.
+ */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+
+module.exports = app;
